7726f90a03ef978b90da14989abd141364faa954
Partha Bhowmik <iamparthabhowmik@gmail.com>
2024-01-04 00:20:30 +0530

    this is the course work backend_02

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/jokes').then((res) => {
+      setJokes(res.data)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  })
+
+  return (
+    <>
+      <h1>Full stack</h1>
+      <p>Jokes: {jokes.length}</p>
+
+      {
+        jokes.map((joke) => (
+          <div key={joke.id}>
+            <h2>{joke.title}</h2>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+export default App
